9d604159bd1f26c5e338575e0059f13fcca21482
kushalashok <kushal.ashok@hsbc.com.cn>
2020-03-31 13:53:05 +0800

    Initial Commit

---
+++ Color Namer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Color Namer
+//
+//  Created by Kushal Ashok on 2020/3/31.
+//  Copyright © 2020 com.kushalashok. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Color Namer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Color Namer
+//
+//  Created by Kushal Ashok on 2020/3/31.
+//  Copyright © 2020 com.kushalashok. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
